a4a056c85185532729e5a7f2e80fdde9264bcc0c
Jonah Draves <jdraves721@gmail.com>
2023-10-25 11:36:34 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def encode(password):
+    encoded = ''
+    for char in password:
+        if int(char) + 3 <= 9:
+            encoded += str(int(char) + 3)
+        else:
+            encoded += str(int(char) - 7)
+    return encoded
+def main():
+    user_input = 0
+    while user_input != 3:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print()
+
+        user_input = int(input('Please enter an option: '))
+        if user_input == 1:
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+if __name__ == '__main__':
+    main()
